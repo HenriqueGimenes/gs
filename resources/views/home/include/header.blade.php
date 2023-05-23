e748946ee1784982b720b6924929c935f0761615
Henrique Gimenes <henrique.gimcruz@gmail.com>
2023-05-23 09:48:58 -0400

    outline

--- resources/views/home/include/header.blade.php
+++ resources/views/home/include/header.blade.php
@@ -89,7 +89,7 @@
             <div class="logo">
                 <a href="{{ route('home') }}">Gs111</a>
             </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            <button style="outline: none;"class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
                 aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
